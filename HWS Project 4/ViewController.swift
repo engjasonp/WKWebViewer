a48230c793951bae751c4d8bb2d3c07cf20b8130
engjasonp <engjasonp@gmail.com>
2015-07-15 16:55:35 -0700

    Refactored code: Made progress bar to visualize estimated page-loading progress. Inserted websites into an array "websites". Added function webView decidePolicyForNavigationAction to check if URL has a host.

--- HWS Project 4/ViewController.swift
+++ HWS Project 4/ViewController.swift
@@ -11,6 +11,8 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com", "slashdot.org"]
     
     override func loadView() {
         webView = WKWebView()
@@ -19,21 +21,27 @@ class ViewController: UIViewController, WKNavigationDelegate {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "http://www.google.com")!
+        let url = NSURL(string: "http://" + websites[0])!
         webView.loadRequest(NSURLRequest(URL: url))
         webView.allowsBackForwardNavigationGestures = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .New, context: nil)
+        progressView = UIProgressView(progressViewStyle: .Default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
         let spacer = UIBarButtonItem(barButtonSystemItem: .FlexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .Refresh, target: webView, action: "reload")
+
         
-        toolbarItems = [spacer, refresh]
+        toolbarItems = [progressButton, spacer, refresh]
         navigationController?.toolbarHidden = false
     }
 
     func openTapped() {
         let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .ActionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "slashdot.org", style: .Default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .Default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
         presentViewController(ac, animated: true, completion: nil)
     }
@@ -47,9 +55,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         title = webView.title
     }
     
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.URL
+        
+        if let host = url!.host {
+            for website in websites {
+                if host.rangeOfString(website) != nil {
+                    decisionHandler(.Allow)
+                    return
+                }
+            }
+        }
+        
+        decisionHandler(.Cancel)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject: AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     func refreshTapped() {
